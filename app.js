852dde20b91cdca920cf69bc63b91a04dc28e60b
Byamba-Ochir J <zaak.zaku@gmail.com>
2021-09-26 16:54:56 +0900

    Update app.js
    renamed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app  = express()
 
 app.get('/', function(req,res){
-  res.send('WORKED')
+  res.send('WORKED...')
 })
 
 app.listen(process.env.PORT || 5000)
